d252942b544b5d644c6a3cfd3eff1692eba1a25d
silentwasd <silentwasd@gmail.com>
2022-06-22 19:40:27 +0300

    Searching by content from api.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div class="container px-4 px-lg-5 py-5">
       <div class="row">
           <div class="col d-flex flex-column gap-3">
-              <PaginationBar :data="pagination" @nextPage="nextPage" @previousPage="previousPage"></PaginationBar>
+              <PaginationBar v-if="pagination.total >= pagination.per_page"
+                             :data="pagination"
+                             @nextPage="nextPage"
+                             @previousPage="previousPage"></PaginationBar>
 
               <div class="row" v-for="(item, index) in sortedItems" :key="index">
                   <div class="col">
@@ -14,7 +17,10 @@
                   </div>
               </div>
 
-              <PaginationBar :data="pagination" @nextPage="nextPage" @previousPage="previousPage"></PaginationBar>
+              <PaginationBar v-if="pagination.total >= pagination.per_page"
+                             :data="pagination"
+                             @nextPage="nextPage"
+                             @previousPage="previousPage"></PaginationBar>
           </div>
 
           <div class="col-4 ps-5">
@@ -24,7 +30,7 @@
                   <div class="mb-3">
                       <label for="type" class="form-label">Хочу:</label>
                       <select id="type" class="form-control" v-model="type">
-                          <option value="">все</option>
+                          <option value="">не важно</option>
                           <option value="sell">купить</option>
                           <option value="buy">продать</option>
                           <option value="rent">снять</option>
@@ -32,6 +38,12 @@
                           <option value="service">получить услугу</option>
                           <option value="want-service">оказать услугу</option>
                       </select>
+                  </div>
+
+                  <div class="mb-3">
+                      <label for="content" class="form-label">Что:</label>
+                      <input id="content" class="form-control" v-model="content"
+                             placeholder="iPhone в отличном состоянии">
                   </div>
               </div>
           </div>
@@ -54,7 +66,8 @@ export default {
     data() {
         return {
             pagination: {},
-            type: ''
+            type: '',
+            content: ''
         };
     },
     computed: {
@@ -69,6 +82,11 @@ export default {
     },
     watch: {
         type() {
+            this.pagination.current_page = 1;
+            this.requestPage();
+        },
+
+        content() {
             this.pagination.current_page = 1;
             this.requestPage();
         }
@@ -88,7 +106,8 @@ export default {
             try {
                 const query = new URLSearchParams({
                     page: this.pagination.current_page,
-                    type: this.type
+                    type: this.type,
+                    content: this.content
                 });
 
                 const response = await axios.get(`http://localhost:8000/api/board/items?${query}`);
