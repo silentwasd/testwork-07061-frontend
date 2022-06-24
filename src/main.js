d3562c490bf2d11cb840d4821f58ffdd0db816d3
silentwasd <silentwasd@gmail.com>
2022-06-24 10:30:11 +0300

    Added guard before enter to some pages.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
+import Cookie from 'js-cookie';
 import App from './App.vue'
 import BoardPage from "@/pages/BoardPage";
 import HelloWorld from "@/components/HelloWorld";
@@ -16,13 +17,21 @@ const routes = [
     { path: '/hello-world', component: HelloWorld },
     { path: '/login', component: LoginPage },
     { path: '/register', component: RegisterPage },
-    { path: '/board/create', component: BoardCreatePage },
+    { path: '/board/create', component: BoardCreatePage, meta: { auth: true } },
     { path: '/board/item/:item', name: 'board.item', component: BoardItemPage, props: true }
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to) => {
+    if (to.meta.auth && !Cookie.get('auth_token')) {
+        return {
+            path: '/login'
+        }
+    }
 })
 
 createApp(App)
